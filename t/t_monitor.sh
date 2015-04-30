1d5e676c8570039166425ae3fd9e0a226cc1694e
mori.takashi <takashi@icicleforest.com>
2015-04-30 21:32:44 +0900

    Update

--- t/t_monitor.sh
+++ t/t_monitor.sh
@@ -18,7 +18,25 @@ do
     _stat=$(Monitor::Process::GetStat ${_p})
     if [ $? = 0 ]
     then
-        echo "# ${_p} $(Monitor::Process::GetUsedPhysicalMemorySize ${_p})"
+        #_pid=$(Monitor::Process::GetPidByStat "${_stat}")
+        #_cmd=$(Monitor::Process::GetCommand ${_stat})
+
+        _ppid=$(Monitor::Process::GetParentPid ${_stat})
+        #_pstat=$(Monitor::Process::GetStat ${_ppid})
+        #_pcmd=$(Monitor::Process::GetCommand ${_pstat})
+
+        _pm=$(Monitor::Process::GetUsedPhysicalMemorySize ${_stat})
+        _vm=$(Monitor::Process::GetUsedVirtualMemorySize ${_stat})
+        #Monitor::Process::GetUsedSwapSize ${_stat}
+
+        ((_pm = _pm / 1024))
+        ((_vm = _vm / 1024))
+
+        echo -e "# PID=${_p} \tPPID=${_ppid}"
+        #echo -e "#\tCMD=${_cmd}\tPCMD=${_pcmd}"
+        echo -e "#\tPM=${_pm}KB"
+        echo -e "#\tVM=${_vm}KB"
+        #echo -e "#\tSW=${_sw}KB"
     fi
 done
 
